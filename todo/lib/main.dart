633802341b5f392401a8d9ed5945e59f08c166a2
Kailany <kailany@alphacode.com.br-at-503688510464>
2020-06-16 22:52:51 -0300

    Criando ListView na HomePage

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart'; // referência do layout a ser utlizado na aplicação (android ou ios);
+import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart'; // referência do layout a ser utlizado na aplicação (android ou ios);
 
 void main() {
   runApp(MyApp());
@@ -19,21 +20,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  // método construtor
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Scaffold - faz com que o Widget assume o estilo de uma página
-      appBar: AppBar(
-        title: Center(
-          child: Text("Todo List"),
+        // Scaffold - faz com que o Widget assume o estilo de uma página
+        appBar: AppBar(
+          title: Center(
+            child: Text("Todo List"),
+          ),
         ),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Olá mundo"),
-        ),
-      ),
-    );
+        body: ListView.builder(
+          itemCount: widget.items.length, // tamanho da lista
+          itemBuilder: (BuildContext context, int index) {
+            // função para definir como os items serão montados na tela
+            return Text(widget.items[index].title);
+          },
+        ));
   }
 }
